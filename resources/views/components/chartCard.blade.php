7f8f9570314468b5481e521bb2c6ed293a4a1b80
Saurabh Jain <saurabhs.jain4u@gmail.com>
2024-08-23 11:17:06 +0530

    chartcard created as different component

---
+++ resources/views/components/chartCard.blade.php
@@ -0,0 +1,7 @@
+<div class="chart-card">
+    <div class="card-header">
+        <div class="chart-title">{{$cardTitle}}</div>
+        <div class="chart-subtitle">{{$cardDescription}}</div>
+    </div>
+    <div id="{{$id}}" class="chart-wrapper"></div>
+</div>
--- resources/views/saurabh.blade.php
+++ resources/views/saurabh.blade.php
@@ -3,13 +3,7 @@
 @section('content')
 <div class="charts-container grid grid-cols-2 lg:grid-cols-3 gap-4">
     <div class="col-12 col-md-4">
-        <div class="chart-card">
-            <div class="card-header">
-                <div class="chart-title">Chart title goes here</div>
-                <div class="chart-subtitle">15 April - 21 April</div>
-            </div>
-            <div id="chart-1" class="chart-wrapper"></div>
-        </div>
+        @include('components.chartCard', ['cardTitle'=>'Chart title goes heresss', 'cardDescription'=>'15 April - 21 April', 'id'=>'chart-1'])
     </div>
     <div class="col-12 col-md-4">
         <div class="chart-card">
@@ -39,31 +33,13 @@
         </div>
     </div>
     <div class="col-12 col-md-4">
-        <div class="chart-card">
-            <div class="card-header">
-                <div class="chart-title">Chart title goes here</div>
-                <div class="chart-subtitle">15 April - 21 April</div>
-            </div>
-            <div id="chart-3" class="chart-wrapper"></div>
-        </div>
+        @include('components.chartCard', ['cardTitle'=>'Chart title goes heresss', 'cardDescription'=>'15 April - 21 April', 'id'=>'chart-3'])
     </div>
     <div class="col-12 col-md-4">
-        <div class="chart-card">
-            <div class="card-header">
-                <div class="chart-title">Chart title goes here</div>
-                <div class="chart-subtitle">15 April - 21 April</div>
-            </div>
-            <div id="chart-4" class="chart-wrapper"></div>
-        </div>
+        @include('components.chartCard', ['cardTitle'=>'Chart title goes heresss', 'cardDescription'=>'15 April - 21 April', 'id'=>'chart-4'])
     </div>
     <div class="col-12 col-md-4">
-        <div class="chart-card">
-            <div class="card-header">
-                <div class="chart-title">Chart title goes here</div>
-                <div class="chart-subtitle">15 April - 21 April</div>
-            </div>
-            <div id="chart-5" class="chart-wrapper"></div>
-        </div>
+        @include('components.chartCard', ['cardTitle'=>'Chart title goes heresss', 'cardDescription'=>'15 April - 21 April', 'id'=>'chart-5'])
     </div>
 </div>
 @endsection
